6ca8e23d028d5fd28ab34aabfea3d58e1fbe225b
ula <urszulaglowacka84@gmail.com>
2023-07-07 20:51:32 +0200

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isfemale = true;
+int age1 = 33;
+
+if (isfemale)
+{
+    if (age1 == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age1 < 30)
+    { Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else
+{
+    if (age1 < 18);
+    Console.WriteLine("Niepełnoletni mężczyzna"); 
+}
+
+
+
+
